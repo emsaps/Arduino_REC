a9d723e6804e8d891569cb5b13e771ff39984e56
Wolfbanus <loreurbane@gmail.com>
2025-09-22 00:15:38 +0200

    Refactor LED state management and implement debounced button checks

--- Arduino_sketches/sketch_REC_pulsation.ino
+++ Arduino_sketches/sketch_REC_pulsation.ino
@@ -1,17 +1,38 @@
 #include <Adafruit_NeoPixel.h>
 
+
 #define LED_PIN     6    // The pin connected to the LED strip
 #define NUM_LEDS    78   // Number of LEDs in your strip
 #define BUTTON_1    2    // Pin for the first button
 #define BUTTON_2    3    // Pin for the second button
 #define BUTTON_3    4    // Pin for the third button
 
+// Debounce settings
+#define DEBOUNCE_DELAY 50 // ms
+
+
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-int currentState = -1;  // -1 represents the initial off state
+// Enum for states
+enum LedState {
+  STATE_OFF = -1,
+  STATE_ONE = 0,
+  STATE_TWO = 1,
+  STATE_THREE = 2
+};
+
+LedState currentState = STATE_OFF;
 int brightness = 0;
 bool increasing = true;
 bool stateChanged = false;
+
+// Debounce variables
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+unsigned long lastDebounceTime3 = 0;
+int lastButtonState1 = HIGH;
+int lastButtonState2 = HIGH;
+int lastButtonState3 = HIGH;
 
 // Define colors
 uint32_t OFF, WHITE, PALE_WHITE, GREEN, BLUE, RED, ORANGE;
@@ -19,7 +40,7 @@ uint32_t OFF, WHITE, PALE_WHITE, GREEN, BLUE, RED, ORANGE;
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  
+
   pinMode(BUTTON_1, INPUT_PULLUP);
   pinMode(BUTTON_2, INPUT_PULLUP);
   pinMode(BUTTON_3, INPUT_PULLUP);
@@ -34,25 +55,58 @@ void setup() {
   ORANGE = strip.Color(255, 165, 0);
 }
 
+
 void loop() {
-  checkButtons();
+  checkButtonsDebounced();
   updateBrightness();
   updateLEDs();
   strip.show();
   delay(10); // Small delay to control the speed of brightness change
 }
 
-void checkButtons() {
-  if (digitalRead(BUTTON_1) == LOW && currentState != 0) {
-    currentState = 0;
-    stateChanged = true;
-  } else if (digitalRead(BUTTON_2) == LOW && currentState != 1) {
-    currentState = 1;
-    stateChanged = true;
-  } else if (digitalRead(BUTTON_3) == LOW && currentState != 2) {
-    currentState = 2;
-    stateChanged = true;
+
+// Debounced button check
+void checkButtonsDebounced() {
+  int reading1 = digitalRead(BUTTON_1);
+  int reading2 = digitalRead(BUTTON_2);
+  int reading3 = digitalRead(BUTTON_3);
+  unsigned long now = millis();
+
+  // Button 1
+  if (reading1 != lastButtonState1) {
+    lastDebounceTime1 = now;
   }
+  if ((now - lastDebounceTime1) > DEBOUNCE_DELAY) {
+    if (reading1 == LOW && currentState != STATE_ONE) {
+      currentState = STATE_ONE;
+      stateChanged = true;
+    }
+  }
+  lastButtonState1 = reading1;
+
+  // Button 2
+  if (reading2 != lastButtonState2) {
+    lastDebounceTime2 = now;
+  }
+  if ((now - lastDebounceTime2) > DEBOUNCE_DELAY) {
+    if (reading2 == LOW && currentState != STATE_TWO) {
+      currentState = STATE_TWO;
+      stateChanged = true;
+    }
+  }
+  lastButtonState2 = reading2;
+
+  // Button 3
+  if (reading3 != lastButtonState3) {
+    lastDebounceTime3 = now;
+  }
+  if ((now - lastDebounceTime3) > DEBOUNCE_DELAY) {
+    if (reading3 == LOW && currentState != STATE_THREE) {
+      currentState = STATE_THREE;
+      stateChanged = true;
+    }
+  }
+  lastButtonState3 = reading3;
 
   if (stateChanged) {
     strip.clear();
@@ -74,15 +128,16 @@ void updateBrightness() {
   }
 }
 
+// Update LEDs based on the current state
 void updateLEDs() {
   switch (currentState) {
-    case 0:
+    case STATE_ONE:
       setStateOne();
       break;
-    case 1:
+    case STATE_TWO:
       setStateTwo();
       break;
-    case 2:
+    case STATE_THREE:
       setStateThree();
       break;
     default:
@@ -112,27 +167,30 @@ void setStateThree() {
   setColor({8, 12, 26, 28, 33, 35, 40, 51}, RED);
 }
 
+// Set color to a single LED, with brightness scaling (unless OFF)
 void setColor(int ledIndex, uint32_t color) {
-  if (color == OFF) {
+  if (color == OFF || brightness == 0) {
     strip.setPixelColor(ledIndex, OFF);
-  } else {
-    uint8_t r = (uint8_t)(color >> 16);
-    uint8_t g = (uint8_t)(color >> 8);
-    uint8_t b = (uint8_t)color;
-    strip.setPixelColor(ledIndex, strip.Color(
-      (r * brightness) / 255,
-      (g * brightness) / 255,
-      (b * brightness) / 255
-    ));
+    return;
   }
+  uint8_t r = (uint8_t)(color >> 16);
+  uint8_t g = (uint8_t)(color >> 8);
+  uint8_t b = (uint8_t)color;
+  strip.setPixelColor(ledIndex, strip.Color(
+    (r * brightness) / 255,
+    (g * brightness) / 255,
+    (b * brightness) / 255
+  ));
 }
 
+// Set color to a range of LEDs
 void setColor(int startLed, int endLed, uint32_t color) {
   for (int i = startLed; i <= endLed; i++) {
     setColor(i, color);
   }
 }
 
+// Set color to a list of LEDs
 void setColor(std::initializer_list<int> leds, uint32_t color) {
   for (int led : leds) {
     setColor(led, color);
